92bb6eb13af7aace68b3efd992df0e5c42bd6483
DerivativeMarmot <derivativetu@gmail.com>
2022-07-08 15:50:46 -0700

    Before semaphore 	modified:   game.c 	modified:   rogue.c

--- game.c
+++ game.c
@@ -1,19 +1,19 @@
 #include "shmmgr.h"
 
-// sem_t *sem_w_ready;
-// sem_t *sem_r_ready;
-// sem_t *sem_b_ready;
+sem_t *sem_w_ready;
+sem_t *sem_r_ready;
+sem_t *sem_b_ready;
 
-/*void semSetup(){
+void semSetup(){
     // unlink all the semaphore before creating them just in case
-    sem_unlink(dungeon_lever_one);
-    sem_unlink(dungeon_lever_two);
-    sem_unlink(ROGUE_SEM);
+    // sem_unlink(dungeon_lever_one);
+    // sem_unlink(dungeon_lever_two);
+    // sem_unlink(ROGUE_SEM);
     sem_unlink(WIZ_READY_SEM);
     sem_unlink(ROG_READY_SEM);
     sem_unlink(BAR_READY_SEM);
     // create semaphores
-    sem_t *sem_w = sem_open(dungeon_lever_one, O_CREAT , 0644, 0);
+    /*sem_t *sem_w = sem_open(dungeon_lever_one, O_CREAT , 0644, 0);
     if (sem_w == SEM_FAILED){
         perror("sem_create/one");
         exit(EXIT_FAILURE);
@@ -27,31 +27,31 @@
     if (sem_b == SEM_FAILED){
         perror("sem_create/two");
         exit(EXIT_FAILURE);
-    }
+    }*/
     sem_w_ready = sem_open(WIZ_READY_SEM, O_CREAT , 0644, 0);
-    if (sem_b == SEM_FAILED){
+    if (sem_w_ready == SEM_FAILED){
         perror("sem_create/w_ready");
         exit(EXIT_FAILURE);
     }
     sem_r_ready = sem_open(ROG_READY_SEM, O_CREAT , 0644, 0);
-    if (sem_b == SEM_FAILED){
+    if (sem_r_ready == SEM_FAILED){
         perror("sem_create/r_ready");
         exit(EXIT_FAILURE);
     }
     sem_b_ready = sem_open(BAR_READY_SEM, O_CREAT , 0644, 0);
-    if (sem_b == SEM_FAILED){
+    if (sem_b_ready == SEM_FAILED){
         perror("sem_create/b_ready");
         exit(EXIT_FAILURE);
     }
     // close them
-    sem_close(sem_w);
+    /*sem_close(sem_w);
     sem_close(sem_r);
-    sem_close(sem_b);
-}*/
+    sem_close(sem_b);*/
+}
 
 int main(void)
 {
-    //semSetup(); // set up semaphore
+    semSetup(); // set up semaphore
     
     allocShm(createShm(), DUNGEON_SIZE); // create shared memory
 
@@ -71,14 +71,14 @@ int main(void)
         }
     }
     
-    /*sem_wait(sem_w_ready); // wait for wizard to be ready to receive signal (important)
+    sem_wait(sem_w_ready); // wait for wizard to be ready to receive signal (important)
     sem_wait(sem_r_ready); // wait for rogue to be ready to receive signal (important)
     sem_wait(sem_b_ready); // wait for barbarian to be ready to receive signal (important)
     // close these sem, no longer useful
     sem_close(sem_w_ready);
     sem_close(sem_r_ready);
-    sem_close(sem_b_ready);*/
-    sleep(1);
+    sem_close(sem_b_ready);
+    //sleep(1);
 
     RunDungeon(pid[0], pid[1], pid[2]);
 
